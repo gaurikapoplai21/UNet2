cccfbf5d45058f46fcaddfce6c2afe4ebc3456c4
gaurikapoplai21 <43783540+gaurikapoplai21@users.noreply.github.com>
2019-10-10 22:39:46 +0530

    Update data.py

--- data.py
+++ data.py
@@ -156,12 +156,12 @@ class dataProcess(object):
 		print('-'*30)
 		imgs = glob.glob(self.data_path+"/*."+self.img_type)
 		print(len(imgs))
-		imgdatas =  np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
+		imgdatas =  np.ndarray((len(imgs),self.out_rows,self.out_cols,3), dtype=np.uint8)
 		
 		imglabels = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
 		for imgname in imgs:
 			midname = imgname[imgname.rindex("/")+1:]
-			img = load_img(self.data_path + "/" + midname,grayscale = True)
+			img = load_img(self.data_path + "/" + midname)
 			label = load_img(self.label_path + "/" + midname,grayscale = True)
 			img = img_to_array(img)
 			label = img_to_array(label)
@@ -186,10 +186,10 @@ class dataProcess(object):
 		print('-'*30)
 		imgs = glob.glob(self.test_path+"/*."+self.img_type)
 		print(len(imgs))
-		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
+		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,3), dtype=np.uint8)
 		for imgname in imgs:
 			midname = imgname[imgname.rindex("/")+1:]
-			img = load_img(self.test_path + "/" + midname,grayscale = True)
+			img = load_img(self.test_path + "/" + midname)
 			img = img_to_array(img)
 			#img = cv2.imread(self.test_path + "/" + midname,cv2.IMREAD_GRAYSCALE)
 			#img = np.array([img])
